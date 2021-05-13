86e8c8cdba87f453f0f3077ad02ee56ca1639aa1
Maxwell Kenned <kenned123@gmail.com>
2021-05-13 09:30:46 -0300

    fix some descriptions and add config to jest and storybook work with next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Solaria-base UI</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="This is a boilerplate project to be used at the beginning of a new project"
         />
       </Head>
       <GlobalStyles />
